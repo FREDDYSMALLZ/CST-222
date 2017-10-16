32bc643c1bf541fa1d0da88ecf24effa30cda040
Caleb Miller <calebgmiller@gmail.com>
2017-10-15 23:21:26 -0700

    Update MemoryManagement.c

--- Assignment5/MemoryManagement.c
+++ Assignment5/MemoryManagement.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+
 int main() {
 srand(time(NULL));
 	int r;
 	r=rand()%4096;
 	printf("%i",r);
+	printf(" binary = 0x%08x", r);
+	printf(" hexadecimal = %04x",r);
+	printf(" Number is Shifted By 16 Bits : %d",r << 16);
 return 0;
 }
